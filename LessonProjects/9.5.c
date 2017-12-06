a6c7618e19105d3f8e61f7995aee4a0022862020
TuringCode1 <32813194+TuringCode1@users.noreply.github.com>
2017-12-06 16:55:52 +0800

    add 9.5.c

---
+++ LessonProjects/9.5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int CoutPrime(int xx[], int m)
+{
+	int i, j = 0, n;
+	for (n = 2; n < m; n++)
+	{
+		for (i = 2; i <= n; i++)
+		if (n%i == 0) break;
+		if (i!=n)  xx[j++] = n;
+	}
+	return(j);
+}
+
+void main()
+{
+	int xx[99], m, i;
+	printf("请输入一个大于1的整数 ");
+	scanf_s("%d", &m);
+	for (i = 0; i < CoutPrime(xx, m); i++)
+		printf("%d ", xx[i]);
+}
