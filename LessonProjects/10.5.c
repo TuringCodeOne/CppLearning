efb1cf439b6fa4fb4a59ac80d5c437bd77a29cb7
YogurtPeng <32813194+TuringCodeOne@users.noreply.github.com>
2017-12-18 17:27:36 +0800

    Create 10.5.c

---
+++ LessonProjects/10.5.c
@@ -0,0 +1,22 @@
+#include<iostream>
+int fun(int x, int *pp)
+{
+	int t;
+	for (int i = 1; i <= x; i += 2)
+		if (x%i == 0)
+		{
+			*pp = i;
+			pp++;
+			t++;
+		}
+	return(t);
+}
+int main()
+{
+	int a, array[99] = { 0 };
+	printf("Please input a integer: ");
+	scanf("%d", a);
+	for (int i = 0; i < fun(a, array); i++)
+		printf("%d", array[i]);
+	return 0;
+}
